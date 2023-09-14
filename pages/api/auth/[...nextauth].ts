39768b7ee8117941962c41f9e722713c50b177ea
helix3 <milan@helix3.io>
2023-09-14 21:24:31 +0600

    discord issue fixed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,15 @@ export default NextAuth({
     Providers.Discord({
       clientId:process.env.DISCORD_CLIENT_ID,
       clientSecret:process.env.DISCORD_CLIENT_SECRET
-    })
+    }),
   ],
+  callbacks: {
+    async redirect(url, baseUrl) {
+      console.log('url', url);
+      console.log('baseUrl', baseUrl);
+      console.log('url.startsWith(baseUrl) ? url : baseUrl', url.startsWith(baseUrl) ? baseUrl : url);
+      
+      return url.startsWith(baseUrl) ? baseUrl : url
+      }
+  }
 })
